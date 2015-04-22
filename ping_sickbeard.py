e88a7adb251735186d65a9b30f7dbeaefb309d60
ZATGamer <abarragree@KAPANOPTLKXMCS1.CORP.CHARTERCOM.com>
2015-04-22 11:38:45 -0600

    Adding SB monitor

---
+++ ping_sickbeard.py
@@ -0,0 +1,94 @@
+
+from urllib2 import urlopen, URLError, HTTPError
+import json
+import socket
+import ConfigParser
+import email_notification as email
+
+
+def call_sb(sb_api_key, sb_ip, sb_port, sb_api):
+    try:
+        socket.setdefaulttimeout(5)
+        response = urlopen('http://{}:{}/api/{}/{}'.format(sb_ip, sb_port, sb_api_key, sb_api))
+        return response, True
+
+    except HTTPError, e:
+        return e.code, False
+
+    except URLError, e:
+        return e.reason, False
+
+
+def write_file(cfile, stuff):
+    with open(cfile, 'wb') as configfile:
+        stuff.write(configfile)
+
+
+if __name__ == '__main__':
+    state_file = 'stateful.ini'
+    config_file = 'config.ini'
+    # Read in the config.
+    config = ConfigParser.RawConfigParser()
+    config.read(config_file)
+
+    # Read in state.
+    state = ConfigParser.RawConfigParser()
+    state.read(state_file)
+
+    sb_api_key = config.get('SBInfo', 'sb_api_key')
+    sb_ip = config.get('SBInfo', 'sb_ip')
+    sb_port = config.get('SBInfo', 'sb_port')
+    sb_api = config.get('SBInfo', 'sb_api')
+
+    up = bool
+    failed = int(state.get('sb', 'failed'))
+    notified = int(state.get('sb', 'notified'))
+
+    response, up = call_sb(sb_api_key, sb_ip, sb_port, sb_api)
+
+    if up is True:
+        response2 = json.load(response)
+        test = response2['result']
+
+        if test == 'success':
+            print 'Sickbeard is up'
+            if failed != 0:
+                failed = 0
+                state.set('sb', 'failed', failed)
+                write_file(state_file, state)
+            if notified != 0:
+                notified = 0
+                state.set('sb', 'notified', notified)
+                write_file(state_file, state)
+                subject = 'SickBeard is up -- SUCCESS'
+                body = 'SickBeard has started responding to ping requests.\n' \
+                       'All is good!'
+                email.send_notification(subject, body)
+
+        else:
+            failed += 1
+            state.set('sb', 'failed', failed)
+            if failed >= 5:
+                print 'Sickbeard is DOWN'
+                if notified == 0:
+                    subject = 'SickBeard is DOWN -- FAILURE'
+                    body = 'SickBeard is not responding to API ping requests.\n' \
+                           'Please have a look.'
+                    email.send_notification(subject, body)
+                    notified += 1
+                    state.set('sb', 'notified', notified)
+            write_file(state_file, state)
+
+    else:
+        failed += 1
+        state.set('sb', 'failed', failed)
+        if failed >= 5:
+            print 'Sickbeard is DOWN'
+            if notified == 0:
+                subject = 'SickBeard is DOWN -- FAILURE'
+                body = 'SickBeard is not responding to API ping requests.\n' \
+                       'Please have a look.'
+                email.send_notification(subject, body)
+                notified += 1
+                state.set('sb', 'notified', notified)
+        write_file(state_file, state)
